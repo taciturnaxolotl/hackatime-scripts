c41a534f736440a74ddb7832b9c4f01f97f19c1c
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-12-07 11:23:19 -0500

    feat: add account deletion tool for user data removal from the database

---
+++ delete-user.ts
@@ -0,0 +1,115 @@
+import { Client } from "pg";
+import type { User } from "./types";
+import { intro, log, text, confirm, spinner, outro } from "@clack/prompts";
+import humanize from "humanize-plus";
+
+const client = new Client(process.env.DATABASE_URL);
+
+intro("Hackatime Account Deletion Tool");
+log.info("This tool will delete user information from the database.");
+
+// check if -u or --user flag with a user id is passed
+const args = process.argv.slice(2);
+let userId = args
+	.find((arg) => arg.match(/^--user=U[0-9A-Z]{10}$/) !== null)
+	?.split("=")[1]
+	.trim();
+
+// if not then ask for one
+if (!userId) {
+	const userIdPrompt = await text({
+		message: "Enter the user id",
+	}).then((res) => res.toString().trim());
+
+	if (userIdPrompt === "") {
+		console.error("Invalid user id");
+		outro("Bye! 👋");
+		process.exit(1);
+	}
+
+	userId = userIdPrompt;
+}
+
+log.step("Fetching data from the db");
+
+try {
+	client.connect();
+
+	const users: User[] = await client
+		.query("SELECT * FROM users WHERE id = $1;", [userId])
+		.then((res) => res.rows);
+
+	const heartbeatCount = await client
+		.query("SELECT COUNT(*) FROM heartbeats WHERE user_id = $1;", [userId])
+		.then((res) => res.rows[0].count);
+
+	console.log(users);
+
+	// confirm this is the user
+	const user = users[0];
+
+	const proceed = await confirm({
+		message: "Is this the user you want to delete?",
+		initialValue: false,
+	});
+
+	if (!proceed) {
+		outro("Not exporting user data");
+		client.end();
+		process.exit(1);
+	}
+
+	// are you sure
+
+	const sure = await confirm({
+		message: `Are you sure you want to delete ${user.name} (${user.email})? This will delete ${humanize.intComma(heartbeatCount)} heartbeats.`,
+		initialValue: false,
+	});
+
+	if (!sure) {
+		outro("Not exporting user data");
+		client.end();
+		process.exit(1);
+	}
+
+	const spin = spinner();
+
+	spin.start("Deleting user");
+
+	// fetch heartbeats
+	await client
+		.query("DELETE FROM aliases WHERE user_id = $1;", [userId])
+		.then((res) => res.rows);
+
+	await client
+		.query("DELETE FROM heartbeats WHERE user_id = $1;", [userId])
+		.then((res) => res.rows);
+
+	await client
+		.query("DELETE FROM summaries WHERE user_id = $1;", [userId])
+		.then((res) => res.rows);
+
+	await client
+		.query("DELETE FROM language_mappings WHERE user_id = $1;", [userId])
+		.then((res) => res.rows);
+
+	await client
+		.query("DELETE FROM project_labels WHERE user_id = $1;", [userId])
+		.then((res) => res.rows);
+
+	await client
+		.query("DELETE FROM leaderboard_items WHERE user_id = $1;", [userId])
+		.then((res) => res.rows);
+
+	await client
+		.query("DELETE FROM users WHERE id = $1;", [userId])
+		.then((res) => res.rows);
+
+	spin.stop("Data Deleted");
+
+	outro("Bye! 👋");
+} catch (error) {
+	console.error(error);
+} finally {
+	client.end();
+}
